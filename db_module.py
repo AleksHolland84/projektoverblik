fae59bd45148af140db74a8a44d1d1fdfa289a07
AleksHolland84 <43878016+AleksHolland84@users.noreply.github.com>
2023-12-30 19:58:15 +0100

    Update db_module.py
    removed caching

--- db_module.py
+++ db_module.py
@@ -41,7 +41,6 @@ def log_book(username: str, label: str, value: str, ref: str, toast: str):
             
 
 # --- Function to add value to the contribution graph ---
-@st.cache_resource # cache the function
 def add_contribution(username: str, value: int = 1, date: str = "2024-12-24"):
     ref = db.reference(f"/content_container/usernames/{username}/graph/data/")
     data = ref.get()
