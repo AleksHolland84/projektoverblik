970f642154d4489210f6e7d64519eedea9597ebf
AleksHolland84 <43878016+AleksHolland84@users.noreply.github.com>
2023-12-30 19:47:41 +0100

    Update db_module.py

--- db_module.py
+++ db_module.py
@@ -42,7 +42,7 @@ def log_book(username: str, label: str, value: str, ref: str, toast: str):
 
 # --- Function to add value to the contribution graph ---
 @st.cache_resource # cache the function
-def add_contribution(username: str, value: int = 1, date: str):
+def add_contribution(username: str, value: int = 1, date):
     ref = db.reference(f"/content_container/usernames/{username}/graph/data/")
     data = ref.get()
     value_of_contribution = data.get(date)
