1cdeb6fd5814a02a6f745369d02819bc31db826f
AleksHolland84 <43878016+AleksHolland84@users.noreply.github.com>
2023-12-30 19:51:49 +0100

    Update app.py
    removed username argument i text_area()

--- app.py
+++ app.py
@@ -158,13 +158,13 @@ if authentication_status and username != "admin":
 
 
     st.header("Uge 3", divider = "grey")
-    if text_area(username=username, label="Gruppekontrakt", value=user_content.get('gruppekontrakt'), ref=f"/content_container/usernames/{username}/content", toast="Gruppekontrakt uploaded!"):
+    if text_area(label="Gruppekontrakt", value=user_content.get('gruppekontrakt'), ref=f"/content_container/usernames/{username}/content", toast="Gruppekontrakt uploaded!"):
         add_contribution(username=username, value=1, date = datetime.now().strftime("%Y-%m-%d")) # add contribution to graph
 
-    if text_area(username=username, label="Undersøgelsesspørgsmål", value=user_content.get('undersøgelsesspørgsmål'), ref=f"/content_container/usernames/{username}/content", toast="Undersøgelsesspørgsmål uploaded!"):
-        add_contribution(username=username, value=1, date = datetime.now().strftime("%Y-%m-%d")) # add contribution to graph
+    if text_area(label="Undersøgelsesspørgsmål", value=user_content.get('undersøgelsesspørgsmål'), ref=f"/content_container/usernames/{username}/content", toast="Undersøgelsesspørgsmål uploaded!"):
+        add_contribution(value=1, date = datetime.now().strftime("%Y-%m-%d")) # add contribution to graph
 
-    if text_area(username=username, label="Problemformulering", value=user_content.get('problemformulering'), ref=f"/content_container/usernames/{username}/content", toast="Problemformulering uploaded!"):
+    if text_area(label="Problemformulering", value=user_content.get('problemformulering'), ref=f"/content_container/usernames/{username}/content", toast="Problemformulering uploaded!"):
         add_contribution(username=username, value=1, date = datetime.now().strftime("%Y-%m-%d")) # add contribution to graph
 
 
